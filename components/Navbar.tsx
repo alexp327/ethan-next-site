265715782e8fdf13aa9bb09add68475ce45f4d5c
Alex Perry <alex.perry422@gmail.com>
2023-05-24 00:25:00 -0400

    add blurred gradient section

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -14,7 +14,7 @@ const Navbar = () => {
         >
           <path d='M5 13.18v4L12 21l7-3.82v-4L12 17l-7-3.82zM12 3L1 9l11 6 9-4.91V17h2V9L12 3z' />
         </svg>
-        <h1 className='px-4 min-w-fit my-auto'>Ethan's Personal Website</h1>
+        <h1 className='px-4 min-w-fit my-auto'>Ethan Perry</h1>
         <nav className='w-full my-auto'>
           <ul className='flex justify-end'>
             <li>
--- components/Skills.tsx
+++ components/Skills.tsx
@@ -2,10 +2,11 @@ import React from "react";
 
 const Skills = () => {
   return (
-    <section id='skills' tabIndex={-1}>
-      {/* <!-- Skills --> */}
-      <h1>My Skills</h1>
-      <h3></h3>
+    <section id='skills' tabIndex={-1} className='min-h-screen'>
+      <div className='absolute bg-[url(../public/oval_aca.png)] bg-no-repeat bg-center blur-3xl w-full h-full m-auto bg-[length:250px] md:bg-[length:400px] lg:bg-[length:600px]'>
+        <h1>My Skills</h1>
+        <h3></h3>
+      </div>
     </section>
   );
 };
