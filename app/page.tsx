8c0061afd6b52067226be87e996c69e5667eb6cc
Alex Perry <alex.perry422@gmail.com>
2023-05-31 00:17:07 -0400

    make skills section grid

--- app/page.tsx
+++ app/page.tsx
@@ -52,44 +52,6 @@ export default function Home() {
         />
       </section>
       <Skills />
-      <div className='px-4 min-h-screen flex flex-col bg-primary-text'>
-        <CTAButton
-          className='mt-4'
-          text={"Primary CTA"}
-          secondaryColor={false}
-        />
-        <CTAButton
-          className='mt-4'
-          text={"Secondary CTA"}
-          secondaryColor={true}
-        />
-        <GhostButton
-          className='mt-4'
-          text={"Primary Ghost"}
-          secondaryColor={false}
-        />
-        <GhostButton
-          className='mt-4'
-          text={"Secondary Ghost"}
-          secondaryColor={true}
-        />
-        <TextButton
-          className='mt-4'
-          text={"Primary Text"}
-          secondaryColor={false}
-        />
-        <TextButton
-          className='mt-4'
-          text={"Secondary Text"}
-          secondaryColor={true}
-        />
-        <TextButton
-          href={"/"}
-          className='mt-4'
-          text={"Primary Text Link Button"}
-          secondaryColor={false}
-        />
-      </div>
     </main>
   );
 }
--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,18 +1,113 @@
 import React from "react";
+import { IoBeerOutline } from "react-icons/io5";
 
 const Skills = () => {
   return (
-    <section id='skills' tabIndex={-1} className='min-h-screen'>
-      {/* <div
-        className='absolute bg-[url(../public/oval_aca.png)] bg-no-repeat bg-center blur-3xl w-full h-full m-auto bg-[length:250px] md:bg-[length:400px] lg:bg-[length:600px]'
-        style={{
-          ["mask-image" as any]: "linear-gradient(black, transparent)",
-          ["-webkit-mask-image" as any]: "linear-gradient(black, transparent)",
-        }}
-      > */}
-      <h1>My Skills</h1>
-      <h3></h3>
-      {/* </div> */}
+    <section id='skills' tabIndex={-1}>
+      <h1 className='pt-16 text-5xl text-center'>My Skills</h1>
+      <div className='h-full p-4 md:px-16 max-w-screen-xl mx-auto pt-8 pb-12'>
+        <ul className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8'>
+          <li className='flex flex-col items-center bg-primary-bg rounded-md shadow-md hover:shadow-lg p-6 hover:scale-105 transition-all'>
+            <div className='bg-primary-accent text-4xl w-fit mx-auto p-3 rounded-full text-primary-bg'>
+              <IoBeerOutline />
+            </div>
+            <h3 className='text-primary-text text-2xl w-fit mx-auto mt-6 text-center'>
+              Some Cool Skill
+            </h3>
+            <p className='text-primary-text w-fit mx-auto mt-6 text-center'>
+              This skill helps me to do x, y, and z.
+            </p>
+          </li>
+          <li className='flex flex-col items-center bg-primary-bg rounded-md shadow-md hover:shadow-lg p-6 hover:scale-105 transition-all'>
+            <div className='bg-primary-accent text-4xl w-fit mx-auto p-3 rounded-full text-primary-bg'>
+              <IoBeerOutline />
+            </div>
+            <h3 className='text-primary-text text-2xl w-fit mx-auto mt-6 text-center'>
+              Some Cool Skill
+            </h3>
+            <p className='text-primary-text w-fit mx-auto mt-6 text-center'>
+              This skill helps me to do x, y, and z.
+            </p>
+          </li>
+          <li className='flex flex-col items-center bg-primary-bg rounded-md shadow-md hover:shadow-lg p-6 hover:scale-105 transition-all'>
+            <div className='bg-primary-accent text-4xl w-fit mx-auto p-3 rounded-full text-primary-bg'>
+              <IoBeerOutline />
+            </div>
+            <h3 className='text-primary-text text-2xl w-fit mx-auto mt-6 text-center'>
+              Some Cool Skill
+            </h3>
+            <p className='text-primary-text w-fit mx-auto mt-6 text-center'>
+              This skill helps me to do x, y, and z.
+            </p>
+          </li>
+          <li className='flex flex-col items-center bg-primary-bg rounded-md shadow-md hover:shadow-lg p-6 hover:scale-105 transition-all'>
+            <div className='bg-primary-accent text-4xl w-fit mx-auto p-3 rounded-full text-primary-bg'>
+              <IoBeerOutline />
+            </div>
+            <h3 className='text-primary-text text-2xl w-fit mx-auto mt-6 text-center'>
+              Some Cool Skill
+            </h3>
+            <p className='text-primary-text w-fit mx-auto mt-6 text-center'>
+              This skill helps me to do x, y, and z.
+            </p>
+          </li>
+          <li className='flex flex-col items-center bg-primary-bg rounded-md shadow-md hover:shadow-lg p-6 hover:scale-105 transition-all'>
+            <div className='bg-primary-accent text-4xl w-fit mx-auto p-3 rounded-full text-primary-bg'>
+              <IoBeerOutline />
+            </div>
+            <h3 className='text-primary-text text-2xl w-fit mx-auto mt-6 text-center'>
+              Some Cool Skill
+            </h3>
+            <p className='text-primary-text w-fit mx-auto mt-6 text-center'>
+              This skill helps me to do x, y, and z.
+            </p>
+          </li>
+          <li className='flex flex-col items-center bg-primary-bg rounded-md shadow-md hover:shadow-lg p-6 hover:scale-105 transition-all'>
+            <div className='bg-primary-accent text-4xl w-fit mx-auto p-3 rounded-full text-primary-bg'>
+              <IoBeerOutline />
+            </div>
+            <h3 className='text-primary-text text-2xl w-fit mx-auto mt-6 text-center'>
+              Some Cool Skill
+            </h3>
+            <p className='text-primary-text w-fit mx-auto mt-6 text-center'>
+              This skill helps me to do x, y, and z.
+            </p>
+          </li>
+          <li className='flex flex-col items-center bg-primary-bg rounded-md shadow-md hover:shadow-lg p-6 hover:scale-105 transition-all'>
+            <div className='bg-primary-accent text-4xl w-fit mx-auto p-3 rounded-full text-primary-bg'>
+              <IoBeerOutline />
+            </div>
+            <h3 className='text-primary-text text-2xl w-fit mx-auto mt-6 text-center'>
+              Some Cool Skill
+            </h3>
+            <p className='text-primary-text w-fit mx-auto mt-6 text-center'>
+              This skill helps me to do x, y, and z.
+            </p>
+          </li>
+          <li className='flex flex-col items-center bg-primary-bg rounded-md shadow-md hover:shadow-lg p-6 hover:scale-105 transition-all'>
+            <div className='bg-primary-accent text-4xl w-fit mx-auto p-3 rounded-full text-primary-bg'>
+              <IoBeerOutline />
+            </div>
+            <h3 className='text-primary-text text-2xl w-fit mx-auto mt-6 text-center'>
+              Some Cool Skill
+            </h3>
+            <p className='text-primary-text w-fit mx-auto mt-6 text-center'>
+              This skill helps me to do x, y, and z.
+            </p>
+          </li>
+          <li className='flex flex-col items-center bg-primary-bg rounded-md shadow-md hover:shadow-lg p-6 hover:scale-105 transition-all'>
+            <div className='bg-primary-accent text-4xl w-fit mx-auto p-3 rounded-full text-primary-bg'>
+              <IoBeerOutline />
+            </div>
+            <h3 className='text-primary-text text-2xl w-fit mx-auto mt-6 text-center'>
+              Some Cool Skill
+            </h3>
+            <p className='text-primary-text w-fit mx-auto mt-6 text-center'>
+              This skill helps me to do x, y, and z.
+            </p>
+          </li>
+        </ul>
+      </div>
     </section>
   );
 };
